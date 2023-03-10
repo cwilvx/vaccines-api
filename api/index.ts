6af25c987c2fbd16b45e0970446cab767d0d0bff
geoffrey45 <geoffreymungai45@gmail.com>
2023-03-10 17:55:07 +0300

    validate schema

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import vaccines from "../data/vaccines";
-import { Child } from "../interfaces";
+import { Child, validateChild } from "../interfaces";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let db: Child[] = [];
+
+const test_child = {
+  id: 1,
+  child_name: "Kimata",
+  guardian_name: "WaKimata",
+  guardian_phone: 88899,
+  age: 344,
+  location: "Turkana",
+  is_male: true,
+};
 
 app.get("/api", (req, res) => {
   res.send({
@@ -22,6 +32,11 @@ app.get("/api", (req, res) => {
         route: "/api/child",
         description: "Add a single child",
         methods: ["POST"],
+      },
+      {
+        route: "/api/child/test",
+        description: "Get a test child",
+        methods: ["GET"],
       },
       {
         route: "/api/child/:id",
@@ -45,20 +60,45 @@ app.get("/api/vaccines", (req, res) => {
   res.send({ vaccines });
 });
 
+// get all children
+app.get("/api/child", (req, res) => {
+  res.send({ message: "Child ID is required" });
+});
+
 // add child
 app.post("/api/child", (req, res) => {
-  console.log(req.body);
-
   const child = req.body as Child;
   child.id = db.length + 1;
 
+  // check if child is of type Child
+  if (!validateChild(child)) {
+    res.status(400).send({ message: "Invalid child schema" });
+    return;
+  }
+
+  // if child name exists, return error
+  const childExists = db.find((c) => c.child_name === child.child_name);
+  if (childExists) {
+    res
+      .status(400)
+      .send({ message: "Not added! This child is already registered" });
+    return;
+  }
+
   db.push(child);
-  res.send({ message: "Child added successfully" });
+  res.send({ child });
 });
 
 // get child by id
 app.get("/api/child/:id", (req, res) => {
   const id = req.params.id;
+
+  // if id is "test", return test child
+  if (id === "test") {
+    res.send({ child: test_child });
+    return;
+  }
+
   const child = db.find((child) => child.id === parseInt(id));
 
   res.send({ child: child ? child : null });
--- interfaces.ts
+++ interfaces.ts
@@ -1,9 +1,22 @@
 export interface Child {
   id: number;
-  child_name: "jeff";
-  guardian_name: "jeff";
-  guardian_phone: "3983";
-  Age: "6 weeks";
-  Location: "garissa";
-  Gender: "male/female";
+  child_name: string;
+  guardian_name: string;
+  guardian_phone: number;
+  age: number;
+  location: string;
+  is_male: boolean;
+}
+
+export function validateChild(_data: any): _data is Child {
+  // check if all the required fields are present and of the correct type
+  return (
+    typeof _data.id === "number" &&
+    typeof _data.child_name === "string" &&
+    typeof _data.guardian_name === "string" &&
+    typeof _data.guardian_phone === "number" &&
+    typeof _data.age === "number" &&
+    typeof _data.location === "string" &&
+    typeof _data.is_male === "boolean"
+  );
 }
